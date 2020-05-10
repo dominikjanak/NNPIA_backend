27fa0ba6b3ca7da625fb901f33a9d60b28096c42
Dominik Janák <dev@dominikjanak.cz>
2020-05-10 21:16:29 +0200

    Database structure

--- src/main/resources/db/migration/V1__createTable.sql
+++ src/main/resources/db/migration/V1__createTable.sql
@@ -1,14 +1,78 @@
-create table quote
-(
-  id int auto_increment primary key,
-  name varchar(255) null
-);
+CREATE TABLE author (
+    id int(11) NOT NULL,
+    country varchar(3) DEFAULT NULL,
+    firstname varchar(50) NOT NULL,
+    surname varchar(50) NOT NULL
+) DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE category (
+    id int(11) NOT NULL,
+    name varchar(50) NOT NULL
+) DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE quote (
+    id int(11) NOT NULL,
+    global bit(1) NOT NULL,
+    quote varchar(1000) NOT NULL,
+    author_id int(11) NOT NULL,
+    user_id int(11) NOT NULL
+) DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE quote_category (
+    quote_id int(11) NOT NULL,
+    category_id int(11) NOT NULL
+) DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE quote_score (
+    quote_id int(11) NOT NULL,
+    user_id int(11) NOT NULL,
+    score tinyint(4) NOT NULL
+) DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE user (
-    id int auto_increment primary key,
-    email varchar(255) DEFAULT NULL,
-    password varchar(255) DEFAULT NULL,
-    username varchar(255) DEFAULT NULL
-);
+    id int(11) NOT NULL,
+    email varchar(100) DEFAULT NULL,
+    firstname varchar(50) DEFAULT NULL,
+    password varchar(100) DEFAULT NULL,
+    surname varchar(50) DEFAULT NULL,
+    username varchar(35) NOT NULL
+) DEFAULT CHARSET=utf8mb4;
 
+ALTER TABLE author
+    ADD PRIMARY KEY (id),
+    ADD UNIQUE KEY UKr1vqikecxbxj0rjrhmb2kbrrn (firstname,surname);
 
+ALTER TABLE category
+    ADD PRIMARY KEY (id),
+    ADD UNIQUE KEY UK_46ccwnsi9409t36lurvtyljak (name);
+
+ALTER TABLE quote
+    ADD PRIMARY KEY (id),
+    ADD KEY FK55f8tgrl40w4xdhsfvgy2ve4m (author_id),
+    ADD KEY FKmc4aq5ngrmytr53brbl9061md (user_id);
+
+ALTER TABLE quote_category
+    ADD KEY FKcbyjhdc91pn0lxsj066vb03nu (category_id),
+    ADD KEY FKso6yp0aqm4g2jqg5b8yef49gf (quote_id);
+
+ALTER TABLE quote_score
+    ADD PRIMARY KEY (quote_id,user_id),
+    ADD KEY FK7dxkti8jhnurqs4yaol6wu7h8 (user_id);
+
+ALTER TABLE user
+    ADD PRIMARY KEY (id),
+    ADD UNIQUE KEY UK_sb8bbouer5wak8vyiiy4pf2bx (username),
+    ADD UNIQUE KEY UK_ob8kqyqqgmefl0aco34akdtpe (email);
+
+ALTER TABLE author
+    MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+ALTER TABLE category
+    MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+ALTER TABLE quote
+    MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+ALTER TABLE user
+    MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
--- src/main/resources/db/migration/V2__initData.sql
+++ src/main/resources/db/migration/V2__initData.sql
@@ -1,2 +1,44 @@
-INSERT INTO user (id, email, password, username) VALUES
-(1, 'mail@dominikjanak.cz', '$2a$10$qAMPooYm0mlE84tBjgqTC.0W4W8CLx5AAsx0OSvvkS8Fq06Lk5HfW', 'janakdom');
+INSERT INTO author (id, country, firstname, surname) VALUES
+(1, 'USA', 'Albert', 'Einstein'),
+(2, 'UK', 'Oscar', 'Wilde'),
+(3, 'CZ', 'Miroslav', 'Horníček'),
+(4, 'CZ', 'Tomáš Garrigue', 'Masaryk');
+
+INSERT INTO category (id, name) VALUES
+(1, 'Život'),
+(2, 'Lidé'),
+(3, 'Tajemství'),
+(4, 'Chování'),
+(5, 'Víra'),
+(6, 'Vesmír'),
+(7, 'Osamělost'),
+(8, 'Oblečení'),
+(9, 'Úspěch'),
+(10, 'Sny');
+
+INSERT INTO user (id, email, firstname, password, surname, username) VALUES
+(1, 'public@dominikjanak.cz', 'Dominik', '$2a$10$LUJWQmfmnq6ERycvkFnZNu80Zi5TW4sP3Ebm2gowjXVd9BUnrusSy', 'Janák', 'janakdom'),
+(2, 'test@dominikjanak.cz', 'test', '$2a$10$uwBNfy/Pzki8UFGOGpXZeubIZwWu6L2d6kAcCpBsSCANjZvQIpsFW', 'test', 'test'),
+(3, 'user@dominikjanak.cz', 'user', '$2a$10$s0PZaL1iFoqrN0RA3nSZEOVtViSADg1dM43vgIkCgTmLhh1J5uNiW', 'user', 'user');
+
+INSERT INTO quote (id, global, quote, author_id, user_id) VALUES
+(1, b'1', 'Existuje tisíce způsobů, jak zabít čas, ale žádný, jak ho vzkřísit.', 1, 1),
+(2, b'1', 'Někdo mlčí, protože neví, a někdo mlčí, protože ví své.', 2, 1),
+(3, b'1', 'Tož demokracii bychom už měli, teď ještě nějaké ty demokraty.', 4, 1),
+(4, b'1', 'Nejsem náročný, mám velmi jednoduchý vkus. Spokojím se vždy s tím nejlepším.', 3, 1);
+
+INSERT INTO quote_category (quote_id, category_id) VALUES
+(1, 1),
+(1, 4),
+(1, 9),
+(2, 1),
+(2, 2),
+(2, 4),
+(2, 9),
+(3, 1),
+(3, 2),
+(3, 4),
+(4, 1),
+(4, 2),
+(4, 4);
+
