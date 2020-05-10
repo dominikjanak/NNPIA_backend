6fea9c8fad22dbe178282d8b76e0a21c76f10997
Dominik Janák <dev@dominikjanak.cz>
2020-05-10 15:27:25 +0200

    Move author controller

--- src/main/java/cz/janakdom/controller/rest/AuthorController.java
+++ src/main/java/cz/janakdom/controller/rest/AuthorController.java
@@ -1,23 +1,16 @@
-package cz.janakdom.controller.rest.security;
+package cz.janakdom.controller.rest;
 
 import cz.janakdom.model.ApiResponse;
-import cz.janakdom.model.AuthToken;
 import cz.janakdom.model.database.Author;
-import cz.janakdom.model.database.User;
 import cz.janakdom.model.dto.AuthorDto;
-import cz.janakdom.model.dto.UserDto;
 import cz.janakdom.service.AuthorService;
-import cz.janakdom.service.UserService;
-import cz.janakdom.util.JwtUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 
 @CrossOrigin
 @RestController
@@ -30,7 +23,7 @@ public class AuthorController {
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @PostMapping("/")
-    public ApiResponse<Author> save(@RequestBody AuthorDto author){
+    public ApiResponse<Author> saveAuthor(@RequestBody AuthorDto author){
 
         if(authorService.findByName(author.getFirstname(), author.getSurname()) != null){
             return new ApiResponse<>(HttpStatus.NOT_ACCEPTABLE.value(), "ALREADY-EXISTS",null);
@@ -40,24 +33,30 @@ public class AuthorController {
     }
 
     @GetMapping("/")
-    public ApiResponse<List<Author>> list(){
+    public ApiResponse<List<Author>> listAuthor(){
         return new ApiResponse<>(HttpStatus.OK.value(), "SUCCESS", authorService.findAll());
     }
 
     @GetMapping("/{id}")
-    public ApiResponse<Author> getOne(@PathVariable int id){
+    public ApiResponse<Author> getOneAuthor(@PathVariable int id){
         Author author = authorService.findById(id);
         return new ApiResponse<>(HttpStatus.OK.value(), author == null ? "NOT-EXISTS" : "SUCCESS", author);
     }
 
     @PutMapping("/{id}")
-    public ApiResponse<AuthorDto> update(@RequestBody AuthorDto authorDto, @PathVariable int id) {
+    public ApiResponse<AuthorDto> updateAuthor(@RequestBody AuthorDto authorDto, @PathVariable int id) {
+        Author newTest = authorService.findByName(authorDto.getFirstname(), authorDto.getSurname());
+
+        if(newTest != null){
+            return new ApiResponse<>(HttpStatus.OK.value(), "ALREADY-EXISTS", null);
+        }
+
         AuthorDto author = authorService.update(id, authorDto);
         return new ApiResponse<>(HttpStatus.OK.value(), author==null ? "NOT-FOUND" : "SUCCESS", author);
     }
 
     @DeleteMapping("/{id}")
-    public ApiResponse<Void> delete(@PathVariable int id) {
+    public ApiResponse<Void> deleteAuthor(@PathVariable int id) {
         Author author = authorService.findById(id);
         if(author != null) authorService.delete(id);
         return new ApiResponse<Void>(HttpStatus.NOT_ACCEPTABLE.value(), author == null ? "NOT-FOUND" : "SUCCESS", null);
