f5eb075c778d7420789de23faab65e341ebbbdbd
Dominik Janák <dev@dominikjanak.cz>
2020-05-10 15:11:04 +0200

    Author

--- src/main/java/cz/janakdom/dao/AuthorDao.java
+++ src/main/java/cz/janakdom/dao/AuthorDao.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AuthorDao extends JpaRepository<Author, Integer> {
+    List<Author> findAllByOrderBySurnameAscFirstnameAsc();
+    Author findByFirstnameAndSurname(String firstname, String surname);
 }
---
+++ src/main/java/cz/janakdom/service/AuthorService.java
@@ -0,0 +1,60 @@
+package cz.janakdom.service;
+
+import cz.janakdom.dao.AuthorDao;
+import cz.janakdom.model.database.Author;
+import cz.janakdom.model.dto.AuthorDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service(value = "authorService")
+public class AuthorService {
+
+    private final AuthorDao dao;
+
+    @Autowired
+    public AuthorService(AuthorDao dao) {
+        this.dao = dao;
+    }
+
+    public List<Author> findAll(){
+        return dao.findAllByOrderBySurnameAscFirstnameAsc();
+    }
+
+    public Author findById(int id) {
+        Optional<Author> optional = dao.findById(id);
+        return optional.orElse(null);
+    }
+
+    public Author findByName(String firstname, String surname) {
+        Optional<Author> optional = Optional.ofNullable(dao.findByFirstnameAndSurname(firstname, surname));
+        return optional.orElse(null);
+    }
+
+    public boolean authorExists(String firstname, String surname) {
+        return findByName(firstname, surname) != null;
+    }
+
+    public void delete(int id) {
+        dao.deleteById(id);
+    }
+
+    public AuthorDto update(Integer id, AuthorDto authorDto) {
+        Author author = findById(id);
+        if(author != null) {
+            BeanUtils.copyProperties(authorDto, author, "id");
+            dao.save(author);
+            return authorDto;
+        }
+        return null;
+    }
+
+    public Author save(AuthorDto author) {
+        Author newAuthor = new Author();
+        BeanUtils.copyProperties(author, newAuthor);
+        return dao.save(newAuthor);
+    }
+}
